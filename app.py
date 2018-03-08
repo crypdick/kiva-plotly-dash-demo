eabeb19f74d95f5bc0c964a5cc758b324594aaef
Richard Decal <richard.decal@ncf.edu>
2018-03-07 22:12:53 -0500

    quarantined beau's code, making it play nice with other graphs, commenting

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 """
 Created on Wed Mar  7 19:18:43 2018
 
-@author: beaubritain
+@author: beaubritain, Richard Decal
 """
 
 import dash
@@ -12,33 +12,55 @@ import dash_html_components as html
 import pandas as pd
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
+import numpy as np
 
-kiva = pd.read_csv('data/kiva_loans.csv')
-#subset for important columns 
-df = kiva[['term_in_months','loan_amount', 'lender_count', 'funded_amount', 'borrower_genders', 'repayment_interval', 'country']]
-#subset for top 2 (number of loans) country values and USA to compare to 
+# Import your dataframe from a csv with pandas
+df = pd.read_csv('data/kiva_loans.csv')
+
+##### Beau graphs
+# subset for important columns
+df = df[['term_in_months', 'loan_amount', 'lender_count', 'funded_amount',
+         'borrower_genders', 'repayment_interval', 'country', 'date']]
+# subset for top 2 (number of loans) country values and USA to compare to
 country_values = ['Philippines', 'Kenya', 'United States']
-df = df[df['country'].isin(country_values)]
-#sample to improve speed
-a = df.sample(5000)
+Beau_df = df[df['country'].isin(country_values)]
+# sample to improve speed
+a = Beau_df.sample(5000)
 
-len(kiva.activity.unique()) #shows there are 163 unique activities
+# len(kiva.activity.unique()) #shows there are 163 unique activities
+
+top5 = df.groupby('activity').size().sort_values(ascending=False)[
+       0:5]  # lets look at top 5
+######## /Beau graphs
 
 
-top5 = kiva.groupby('activity').size().sort_values(ascending=False)[0:5] #lets look at top 10
 
+###### Richard maps
+# converting dates from string to DateTime objects gives nice tools
+df['date'] = pd.to_datetime(df['date'])
+# for example, we can turn the full date into just a year
+df['year'] = df.date.dt.to_period("Y")
+# then convert it to integers so you can do list comprehensions later
+# astype(int) expects a strings, so we need to go Period -> str -> int
+# we want ints so we can find the min, max, etc later
+df['year'] = df.year.astype(str).astype(int)
+
+
+
+######
 
 app = dash.Dash()
 
 app.layout = html.Div(className='container', children=[
-    html.H1(children='Top 3 countries Violin distributions', #add a title
-        style={
-            'textAlign': 'center', # center the header
-            'color': '#7F7F7F' # https://www.biotechnologyforums.com/thread-7742.html more color code options
-        }),
+    html.H1(children='Top 3 countries Violin distributions',  # add a title
+            style={
+                'textAlign': 'center',  # center the header
+                'color': '#7F7F7F'
+            # https://www.biotechnologyforums.com/thread-7742.html more color code options
+            }),
     html.Hr(),
     html.Div(className='two columns', children=[
-        dcc.RadioItems( #buttons that select which y value in violin plots
+        dcc.RadioItems(  # buttons that select which y value in violin plots
             id='items',
             options=[
                 {'label': 'term in months', 'value': 'term_in_months'},
@@ -46,19 +68,20 @@ app.layout = html.Div(className='container', children=[
                 {'label': 'lender count', 'value': 'lender_count'},
                 {'label': 'funded amount', 'value': 'funded_amount'}
             ],
-            value= 'term_in_months',
+            value='term_in_months',
             style={'display': 'block',
                    'textAlign': 'center',
                    'color': '#7FDBFF'}
         ),
         html.Hr(),
-        dcc.RadioItems( #more options
+        dcc.RadioItems(  # more options
             id='points',
             options=[
                 {'label': 'Display All Points', 'value': 'all'},
                 {'label': 'Hide Points', 'value': False},
                 {'label': 'Display Outliers', 'value': 'outliers'},
-                {'label': 'Display Suspected Outliers', 'value': 'suspectedoutliers'},
+                {'label': 'Display Suspected Outliers',
+                 'value': 'suspectedoutliers'},
             ],
             value='all',
             style={'display': 'block',
@@ -70,69 +93,74 @@ app.layout = html.Div(className='container', children=[
     html.H1(
         children='Top 5 activities for loans',
         style={
-            'textAlign': 'center', # center the header
-            'color': '#7F7F7F' # https://www.biotechnologyforums.com/thread-7742.html more color code options
+            'textAlign': 'center',  # center the header
+            'color': '#7F7F7F'
+        # https://www.biotechnologyforums.com/thread-7742.html more color code options
         }
-    ),    
-    html.Div(dcc.Graph( # add a bar graph to dashboard
+    ),
+    html.Div(dcc.Graph(  # add a bar graph to dashboard
         id='basic-interactions',
         figure={
             'data': [
                 {
-                    'x': top5.index, 
+                    'x': top5.index,
                     'y': top5,
                     'type': 'bar',
-                    'opacity': .6 #changes the bar chart's opacity
+                    'opacity': .6  # changes the bar chart's opacity
                 }
-            
+
             ],
             'layout': go.Layout(
-                xaxis={ 'title': 'Activity'},
+                xaxis={'title': 'Activity'},
                 yaxis={'title': 'Count'},
             )
         }
-            
-        
+
     ))
 ])
-        
+
+
 @app.callback(
     Output('graph', 'figure'), [
-    Input('items', 'value'),
-    Input('points', 'value')])
+        Input('items', 'value'),
+        Input('points', 'value')])
 def update_graph(value, points):
     return {
         'data': [
             {
                 'type': 'violin',
-                'x': a['country'][a['country']== 'Philippines'], # specify a violin plot filtered 'value' of data column
+                'x': a['country'][a['country'] == 'Philippines'],
+            # specify a violin plot filtered 'value' of data column
                 'y': a[value],
-                'text': ['Sample {}'.format(i) for i in range(len(df))],
+                'text': ['Sample {}'.format(i) for i in range(len(Beau_df))],
                 'points': points,
-                'jitter': .7 #set the space of points
+                'jitter': .7  # set the space of points
             },
             {
                 'type': 'violin',
-                'x': a['country'][a['country']== 'Kenya'],
+                'x': a['country'][a['country'] == 'Kenya'],
                 'y': a[value],
-                'text': ['Sample {}'.format(i) for i in range(len(df))],
+                'text': ['Sample {}'.format(i) for i in range(len(Beau_df))],
                 'points': points,
                 'jitter': .7
             },
             {
                 'type': 'violin',
-                'x': a['country'][a['country']== 'United States'],
+                'x': a['country'][a['country'] == 'United States'],
                 'y': a[value],
-                'text': ['Sample {}'.format(i) for i in range(len(df))],
+                'text': ['Sample {}'.format(i) for i in range(len(Beau_df))],
                 'points': points,
                 'jitter': .7
-            }  
+            }
         ],
-        
+
         'layout': go.Layout(
-                yaxis={'title': value},
-                showlegend = False #since we listed them as different dictionaries within 'data' they are automatically separated by color with a legend. Remove this
-            )
+            yaxis={'title': value},
+            showlegend=False
+            # since we listed them as different dictionaries within 'data' they are automatically separated by color with a legend. Remove this
+        )
     }
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
